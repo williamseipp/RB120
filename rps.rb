da3f695093e0ecb7e0322e310bc1be54230cdedc
William <william.seipp@gmail.com>
2023-11-29 10:08:12 -0800

    Refactor logic out for whether Player is human/computer by adding subclasses

--- rps.rb
+++ rps.rb
@@ -1,78 +1,61 @@
 class Player
   attr_accessor :move, :name
 
-  def initialize(player_type = :human)
-    @player_type = player_type
-    @move = nil
+  def initialize
     set_name
   end
+end
 
+class Human < Player
   def set_name
-    if human?
-      n = ""
-      loop do
-        puts "What's your name?"
-        n = gets.chomp
-        break unless n.empty?
-        puts "Sorry, must enter a value."
-      end
-      self.name = n
-    else
-      self.name = ['R2D2', 'Hal', 'GlaDOS'].sample
+    n = ''
+    loop do
+      puts "What's your name?"
+      n = gets.chomp
+      break unless n.empty?
+
+      puts 'Sorry, must enter a value.'
     end
+    self.name = n
   end
 
   def choose
-    if human?
-      choice = nil
-      loop do
-        puts "Please choose rock, paper, or scissors:" 
-        choice = gets.chomp
-        break if ['rock', 'paper', 'scissors'].include? choice
-        puts "Sorry, invalid choice."
-      end
-      self.move = choice
-    else
-      # computer chooses from random move
-      self.move = ['rock', 'paper', 'scissors'].sample
+    choice = nil
+    loop do
+      puts 'Please choose rock, paper, or scissors:'
+      choice = gets.chomp
+      break if %w(rock paper scissors).include? choice
+
+      puts 'Sorry, invalid choice.'
     end
-  end
-
-  def human?
-    @player_type == :human
+    self.move = choice
   end
 end
 
-class Move
-  def initialize
-    # a move doesn't have a beginning state, does it?
+class Computer < Player
+  def set_name
+    self.name = %w(R2D2 Hal GlaDOS).sample
   end
-end
 
-class Rule
-  def initialize
-    # dunno about state..
+  def choose
+    self.move = %w(rock paper scissors).sample
   end
-end
-
-# where does this go?
-def compare(move1, move2)
 end
 
 class RPSGame
-
   attr_accessor :human, :computer
+
   def initialize
-    @human = Player.new
-    @computer = Player.new(:computer)
+    @human = Human.new
+    @computer = Computer.new
   end
 
   def display_welcome_message
-    puts "Welcome to Rock, Paper, Scissors!"
+    puts 'Welcome to Rock, Paper, Scissors!'
   end
 
   def display_goodbye_message
-    puts "Thanks for playing Rock, Paper, Scissors. Good bye!"
+    puts 'Thanks for playing Rock, Paper, Scissors. Good bye!'
   end
 
   def display_winner
@@ -85,7 +68,7 @@ class RPSGame
       puts "#{human.name} won!" if computer.move == 'scissors'
       puts "#{computer.name} won!" if computer.move == 'paper'
     when 'paper'
-      puts "It's a tie" if computer.move =='paper'
+      puts "It's a tie" if computer.move == 'paper'
       puts "#{human.name} won!" if computer.move == 'rock'
       puts "#{computer.name} won!" if computer.move == 'scissors'
     when 'scissors'
@@ -98,9 +81,9 @@ class RPSGame
   def play_again?
     answer = nil
     loop do
-      puts "Would you like to play again? (y/n)"
+      puts 'Would you like to play again? (y/n)'
       answer = gets.chomp
-      break if ['y', 'n'].include? answer.downcase
+      break if %w(y n).include? answer.downcase
     end
     answer == 'y'
   end
@@ -108,9 +91,9 @@ class RPSGame
   def play
     display_welcome_message
     loop do
-    human.choose
-    computer.choose
-    display_winner
+      human.choose
+      computer.choose
+      display_winner
       break unless play_again?
     end
     display_goodbye_message
