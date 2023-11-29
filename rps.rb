c94b780c398a0836d8942a13b76ba44be51dff1e
William <william.seipp@gmail.com>
2023-11-29 12:26:07 -0800

    Refactor methods to appease Rubocop!

--- rps.rb
+++ rps.rb
@@ -18,29 +18,15 @@ class Move
   end
 
   def >(other_move)
-    if rock?
-      return true if other_move.scissors?
-      false
-    elsif paper?
-      return true if other_move.rock?
-      false
-    elsif scissors?
-      return true if other_move.paper?
-      false
-    end
+    (rock? && other_move.scissors?) ||
+      (paper? && other_move.rock?) ||
+      (scissors? && other_move.paper?)
   end
 
   def <(other_move)
-    if rock?
-      return true if other_move.paper?
-      false
-    elsif paper?
-      return true if other_move.scissors?
-      false
-    elsif scissors?
-      return true if other_move.rock?
-      false
-    end
+    (rock? && other_move.paper?) ||
+      (paper? && other_move.scissors?) ||
+      (scissors? && other_move.rock?)
   end
 
   def to_s
@@ -107,10 +93,12 @@ class RPSGame
     puts 'Thanks for playing Rock, Paper, Scissors. Good bye!'
   end
 
-  def display_winner
+  def display_moves
     puts "#{human.name} chose #{human.move}"
     puts "#{computer.name} chose #{computer.move}"
+  end
 
+  def display_winner
     if human.move > computer.move
       puts "#{human.name} won!"
     elsif human.move < computer.move
@@ -135,6 +123,7 @@ class RPSGame
     loop do
       human.choose
       computer.choose
+      display_moves
       display_winner
       break unless play_again?
     end
