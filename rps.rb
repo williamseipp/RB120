0690feb26502152dc8565046ab9d55dc1248b070
William <william.seipp@gmail.com>
2023-11-30 10:45:42 -0800

    Refactor winning/losing logic into hashes

--- rps.rb
+++ rps.rb
@@ -1,55 +1,34 @@
-# add lizard and spock to moves
 class Move
+  attr_reader :value
+
   VALUES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+
+  WINNING_COMBINATIONS = {
+    'rock' => ['scissors', 'lizard'],
+    'paper' => ['rock', 'spock'],
+    'scissors' => ['paper', 'lizard'],
+    'lizard' => ['spock', 'paper'],
+    'spock' => ['rock', 'scissors']
+  }
+
+  LOSING_COMBINATIONS = {
+    'rock' => ['paper', 'spock'],
+    'paper' => ['scissors', 'lizard'],
+    'scissors' => ['rock', 'spock'],
+    'lizard' => ['rock', 'scissors'],
+    'spock' => ['paper', 'lizard']
+  }
 
   def initialize(value)
     @value = value
   end
 
-  def rock?
-    @value == 'rock'
-  end
-
-  def paper?
-    @value == 'paper'
-  end
-
-  def scissors?
-    @value == 'scissors'
-  end
-
-  def lizard?
-    @value == 'lizard'
-  end
-
-  def spock?
-    @value == 'spock'
-  end
-
   def >(other_move)
-    (rock? && other_move.scissors?) ||
-      (rock? && other_move.lizard?) ||
-      (paper? && other_move.rock?) ||
-      (paper? && other_move.spock?) ||
-      (scissors? && other_move.paper?) ||
-      (scissors? && other_move.lizard?) ||
-      (lizard? && other_move.paper?) ||
-      (lizard? && other_move.spock?) ||
-      (spock? && other_move.rock?) ||
-      (spock? && other_move.scissors?)
+    WINNING_COMBINATIONS[value].include?(other_move.value)
   end
 
   def <(other_move)
-    (rock? && other_move.paper?) ||
-      (rock? && other_move.spock?) ||
-      (paper? && other_move.scissors?) ||
-      (paper? && other_move.lizard?) ||
-      (scissors? && other_move.rock?) ||
-      (scissors? && other_move.spock?) ||
-      (lizard? && other_move.rock?) ||
-      (lizard? && other_move.scissors?) ||
-      (spock? && other_move.lizard?) ||
-      (spock? && other_move.paper?)
+    LOSING_COMBINATIONS[value].include?(other_move.value)
   end
 
   def to_s
